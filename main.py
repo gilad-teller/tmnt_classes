6d9fe8c69fa244a18f326e098d27e441388bfcd4
Gilad Teller <Gilad.Teller@toluna.com>
2023-03-22 10:17:45 +0200

    feat: add command line argument for directory

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 from lib import words
 from lib import files
-
+import sys
 
 def main():
-    # sentence = input("Sentence to check: ")
-    # isTmnt = words.isTMNT(sentence)
-    # print("Yes" if isTmnt else "No")
-
-    directory = input("Directory: ")
+    if len(sys.argv) > 1:
+        directory = sys.argv[1]
+    else:
+        directory = input("Directory: ")
     onlyFiles = files.getFiles(directory)
     for f in onlyFiles:
         sentence = files.sentenceFromFile(f)
         isTmnt = words.isTMNT(sentence)
         if isTmnt:
-            print(sentence)
+            print(f"{sentence} - {f}")
 
 
 if __name__ == "__main__":
